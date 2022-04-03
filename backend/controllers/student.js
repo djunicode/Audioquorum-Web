5f3e5a7a30b6fea15385fd6bc6fbf4c6c4af0bf5
Rosita <esorthe7@gmail.com>
2022-04-03 22:14:17 +0530

    Add test routes and view student routes

---
+++ backend/controllers/student.js
@@ -0,0 +1,47 @@
+// Import modules
+const User = require("../models/user")
+
+// View a student by their id mentioned in the body of the request
+const viewStudentById = (req, res) => {
+    
+    const id = req.body.id;
+
+    try {
+        // Find user by id
+        const student = await User.findById(id);
+
+        // Send student as data
+        res.status(200).json({
+            data: student
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        }); 
+    }
+    
+}
+
+const viewAllStudents = async (req, res) => {
+
+    try {
+
+        // Find all users with student type
+        const students = await User.find({type: 'STUDENT'});
+
+        // Send students as data
+        res.status(200).json({
+            data: students
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        }); 
+    }
+    
+
+
+}
+
+module.exports = {viewStudentById, viewAllStudents}
---
+++ backend/controllers/test.js
@@ -0,0 +1,163 @@
+const mongoose = require("mongoose");
+const { ObjectId } = require('mongodb');
+
+// Importing modules
+const Test = require('../models/test');
+const Question = require('../models/question');
+
+// Create Event
+const createTest = async (req, res) => {
+    try {
+ 
+        const questionIds = []
+        await Promise.all(req.body.questions.map( async (question) => {
+            let newQuestion = new Question(question);
+            await newQuestion.save();
+            questionIds.push(newQuestion._id);
+        }))
+
+        console.log(questionIds);
+       
+        // EDIT AFTER TESTING
+        const currentUser = req.user ? req.user : {
+            _id:3,
+            name: "Rosita",
+            username: "rosita07",
+            type: "TEACHER",
+            test: [{testId: 1, marksObtained: 5}, {testId: 3, marksObtained: 2}],
+            standard: 3
+        
+        }
+       
+        let newTest = new Test({...req.body, questionIds: questionIds, teacherId: currentUser._id});
+
+        // currentUser.testscreated.push(newTest._id);
+
+        await newTest.save();
+        // await currentUser.save();
+
+        res.status(201).json({
+            message: 'Test created successfully!',
+            data: newTest
+        })
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+}
+
+// Give a test
+const startTest = async (req, res) => {
+    try {
+        const test = await Test.findById(req.body.testId);
+        let currentUser = req.user;
+        
+        if(!test) {
+            res.status(404).json({
+                message: 'Test Not Found!',
+            });
+            return;
+        }
+        
+        
+        currentUser.test.push({testId: test._id, marksObtained: null});
+        test.userIds.push(currentUser._id);
+        
+
+        await test.save();
+        await currentUser.save();
+        
+        res.status(201).json({
+            message: 'Test started successfully!'
+        });
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+
+// End a test
+const endTest = async (req, res) => {
+    try {
+        const test = await Test.findById(req.body.testId);
+        let currentUser = req.user;
+        
+        if(!test) {
+            res.status(404).json({
+                message: 'Test Not Found!',
+            });
+            return;
+        }
+        
+        currentUser.test.push({testId: test._id, marksObtained: req.body.marks});
+        
+        await currentUser.save();
+        
+        res.status(201).json({
+            message: 'Test ended successfully!'
+        });
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+
+
+// View the user's available tests by standard
+
+const getTestsByStandard = async (req, res) => {
+    try {
+        
+        const currentUser = req.user
+        const standard = currentUser.standard
+        let tests = await Test.find({standard});
+
+        res.status(200).json({
+            tests
+        });
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+
+
+// View the user's attempted tests
+
+const getAttemptedTests = async (req, res) => {
+    try {
+
+        const currentUser = req.user;
+
+        const testsGiven = []
+        currentUser.test.map((test) => {
+            testsGiven.push(test.testId)
+        })
+
+        const tests = []
+    
+        await Promise.all (testsGiven.map(async (testId) => {
+            const test = await Test.findById(testId);
+            tests.push(test)
+        }))
+     
+        res.status(200).json({
+            tests
+        });
+
+    } catch(error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+
+module.exports = {createTest, getTestsByStandard, getAttemptedTests, startTest, endTest}
