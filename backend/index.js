794585bb327fada63024f19a2291b5b9c6512f80
elo252 <drg020502@gmail.com>
2022-04-03 23:08:50 +0530

    Added cookie parser

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require('morgan');
 const cors = require("cors");
 const db = require('./connection');
+const cookieParser = require("cookie-parser")
 
 // Initializing an express app
 const app = express();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5001;
 // Formatting incoming data
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 // Logging
 app.use(morgan('dev'));
