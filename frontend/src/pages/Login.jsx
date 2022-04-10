76a21432fdc359b5a23420909ba7fbbcc98ae814
Rosita <esorthe7@gmail.com>
2022-04-10 16:16:42 +0530

    Make Signup Page, Edit Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,60 +1,82 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { InputLabel, FormControl } from '@mui/material';
-import Divider from '@mui/material/Divider';
+// Import React
+import * as React from "react";
+
+// Import Material UI components
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { InputLabel, FormControl } from "@mui/material";
+import Divider from "@mui/material/Divider";
+
+// Import React Router DOM functions
+import {useNavigate} from "react-router-dom";
 
 export const Login = () => {
-  return (
-    <Box  sx={{
-      marginTop: 8,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-     
-    }}>
-      <Paper elevation={6} sx={{
-        padding: 5,
-        borderRadius: 3,
-        borderLeft: "80px solid #30574E",
-        width: "370px",
 
-        }}>
-      <Typography component="h1" variant="h3" color="primary" fontWeight="bold">
-            LOG IN
-      </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={6}
+        sx={{
+          padding: 5,
+          borderRadius: 3,
+          borderLeft: "80px solid #30574E",
+          width: "370px",
+        }}
+      >
+        <Typography
+          component="h1"
+          variant="h3"
+          color="primary"
+          fontWeight="bold"
+        >
+          LOG IN
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
           <FormControl variant="standard" fullWidth>
-          <InputLabel shrink htmlFor="email" sx={{color: "#000", fontWeight: 500, fontSize: 22}}>
-            Username
-          </InputLabel>
-          <TextField
-                sx={{mt: 3}}
-                required
-                margin="normal"
-                fullWidth
-                id="email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                variant="filled"
-                hiddenLabel
-              
-              />
+            <InputLabel
+              shrink
+              htmlFor="username"
+              sx={{ color: "#000", fontWeight: 500, fontSize: 22 }}
+            >
+              Username
+            </InputLabel>
+            <TextField
+              sx={{ mt: 3 }}
+              required
+              margin="normal"
+              fullWidth
+              id="username"
+              name="username"
+              autoFocus
+              variant="filled"
+              hiddenLabel
+            />
           </FormControl>
-          <FormControl variant="standard" fullWidth sx={{mt: 3}}>
-          <InputLabel shrink htmlFor="password" sx={{color: "#000", fontWeight: 500, fontSize: 22}}>
-            Password
-          </InputLabel>
-          <TextField
-              sx={{mt: 3}}
+          <FormControl variant="standard" fullWidth sx={{ mt: 3 }}>
+            <InputLabel
+              shrink
+              htmlFor="password"
+              sx={{ color: "#000", fontWeight: 500, fontSize: 22 }}
+            >
+              Password
+            </InputLabel>
+            <TextField
+              sx={{ mt: 3 }}
               margin="normal"
               required
               fullWidth
@@ -66,70 +88,49 @@ export const Login = () => {
               hiddenLabel
             />
           </FormControl>
-            {/* <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              variant="filled"
-              
-            
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              variant="filled"
-              
-            /> */}
-            <Typography>
-            <Link href="#" variant="body2" sx={{textDecoration: "none"}} color="text.primary">
-                  Forgot password?
+
+          <Typography>
+            <Link
+              href="#"
+              variant="body2"
+              sx={{ textDecoration: "none" }}
+              color="text.primary"
+            >
+              Forgot password?
             </Link>
-            </Typography>
-            <Grid container justifyContent="flex-end">
+          </Typography>
+
+          <Grid container justifyContent="flex-end">
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
               labelPlacement="start"
             />
-            </Grid>
-           
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 1, mb: 2, fontSize: "20px", fontWeight: 600}}
-            >
-              LOG IN
-            </Button>
-           
-            <Divider>
-              <Typography>
-                OR
-              </Typography>
-            </Divider>
-            <Typography sx={{mt: 1}}>
-              New to Audioquorum?
-            </Typography>
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 1, fontSize: "20px", fontWeight: 600}}
-            >
-              SIGN UP
-            </Button>
-          </Box>
+          </Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 1, mb: 2, fontSize: "20px", fontWeight: 600 }}
+          >
+            LOG IN
+          </Button>
+
+          <Divider>
+            <Typography>OR</Typography>
+          </Divider>
+          <Typography sx={{ mt: 1 }}>New to Audioquorum?</Typography>
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{ mt: 1, fontSize: "20px", fontWeight: 600 }}
+            onClick={() => navigate("/signup")}
+          >
+            SIGN UP
+          </Button>
+        </Box>
       </Paper>
     </Box>
-  )
-}
+  );
+};
