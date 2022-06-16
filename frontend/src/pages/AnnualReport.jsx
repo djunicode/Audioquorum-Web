7ca063025d3b9bcadc584bdfcd6c6019fd17d011
Mihir Shinde <mihirushinde29@gmail.com>
2022-06-16 15:24:43 +0530

    Added Dashboard, Annual Report and Quizzes page

--- frontend/src/pages/AnnualReport.jsx
+++ frontend/src/pages/AnnualReport.jsx
@@ -1,7 +1,111 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Box, Grid, Typography } from '@mui/material'
+import { styled } from '@mui/system'
+import axios from 'axios'
+
+const StudentBox = styled(Box)(({theme}) => ({
+  minHeight: '36px',
+  backgroundColor: theme.palette.secondary.main, 
+  borderRadius: '5px',
+  marginTop: '10px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  // cursor: 'pointer',
+  // '&:hover': {
+  //   boxShadow: '4px 4px 4px 0px #00000040;'
+  // }
+}))
+
+const StudentReport = () => {
+  return (
+    <StudentBox>
+      <Grid container>
+        <Grid item xs={1} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography sx={{fontSize: '20px', lineHeight: '30px', fontWeight: 400, marginY: '3px'}}>
+            01
+          </Typography>
+        </Grid>
+        <Grid item xs={4} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography sx={{fontSize: '20px', lineHeight: '30px', fontWeight: 400, marginY: '3px'}}>
+            Student Name
+          </Typography>
+        </Grid>
+        <Grid item xs={3} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography sx={{fontSize: '20px', lineHeight: '30px', fontWeight: 400, marginY: '3px'}}>
+            01
+          </Typography>
+        </Grid>
+        <Grid item xs={3} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography sx={{fontSize: '20px', lineHeight: '30px', fontWeight: 400, marginY: '3px'}}>
+            411
+          </Typography>
+        </Grid>
+        <Grid item xs={1} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography sx={{fontSize: '20px', lineHeight: '30px', fontWeight: 400, marginY: '3px'}}>
+            89.33
+          </Typography>
+        </Grid>
+      </Grid>
+    </StudentBox>
+  )
+}
 
 export const AnnualReport = () => {
+  const [data, setData] = useState([])
+  const token = localStorage.getItem('token')
+  console.log(token);
+  useEffect(() => {
+    axios
+      .get('http://audioquorum-api.herokuapp.com/api/test/view/standard/', {
+        headers: {
+          'Authentication': token
+        }
+      })
+      .then(response => setData(response.data.tests))
+  }, [])
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
   return (
-    <div>AnnualReport</div>
+    <Box sx={{paddingX: '100px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Typography sx={{marginY: '24px', textTransform: 'uppercase', fontWeight: 600, fontSize: '30px', lineHeight: '45px'}}>
+        Annual Report
+      </Typography>
+      <Box sx={{backgroundColor: '#F0F0F0', paddingX: '50px', paddingY: '35px', width: '100%', minHeight: '60vh', marginBottom: '70px'}}>
+        <Grid container>
+          <Grid item xs={1} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Typography sx={{fontSize: '24px', lineHeight: '36px', fontWeight: 500}}>
+              Sr. No.
+            </Typography>
+          </Grid>
+          <Grid item xs={4} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Typography sx={{fontSize: '24px', lineHeight: '36px', fontWeight: 500}}>
+              Student Name
+            </Typography>
+          </Grid>
+          <Grid item xs={3} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Typography sx={{fontSize: '24px', lineHeight: '36px', fontWeight: 500}}>
+              Roll No.
+            </Typography>
+          </Grid>
+          <Grid item xs={3} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Typography sx={{fontSize: '24px', lineHeight: '36px', fontWeight: 500}}>
+              Total Marks
+            </Typography>
+          </Grid>
+          <Grid item xs={1} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Typography sx={{fontSize: '24px', lineHeight: '36px', fontWeight: 500}}>
+              Percentage
+            </Typography>
+          </Grid>
+        </Grid>
+        <StudentReport />
+        <StudentReport />
+        <StudentReport />
+        <StudentReport />
+        <StudentReport />
+      </Box>
+    </Box>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { theme } from '../styles/theme'
+import axios from 'axios'
+
+const Card = ({ data }) => {
+  return(
+    <Box sx={{width: '195px', minHeight: '190px', backgroundColor: theme.palette.secondary.main, borderRadius: '10px', boxShadow: '4px 4px 4px 0px #00000040;', paddingX: '15px', paddingTop: '23px', cursor: 'pointer'}}>
+      <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Box sx={{width: '86px', height: '86px', backgroundColor: 'white', borderRadius: '50%'}}></Box>
+      </Box>
+      <Box sx={{marginTop: '27px', border: '0px', borderTop: '2px', borderColor: 'black', borderStyle: 'solid', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '15px'}}>
+        <Typography sx={{fontWeight: 400, lineHeight: '30px', fontSize: '15px'}}>
+          {data.subject}
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
 
 export const Dashboard = () => {
+  const [data, setData] = useState([])
+  const token = localStorage.getItem('token')
+  console.log(token);
+  useEffect(() => {
+    axios
+      .get('http://audioquorum-api.herokuapp.com/api/test/view/attempted/', {
+        headers: {
+          'Authentication': token
+        }
+      })
+      .then(response => setData(response.data.tests))
+  }, [])
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
   return (
-    <div>Dashboard</div>
+    <Box sx={{paddingX: '6%', paddingY: '80px', display: 'flex', gap: '50px', flexWrap: 'wrap'}}>
+      {data && data.map(test => <Card key={test._id} data={test} />)}
+    </Box>
   )
 }
